362c139ffe0742d17fae065dbaab94182131254b
Youssef Noam <ynoam@e1r1p7.1337.ma>
2019-10-24 20:05:27 +0200

    add ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ynoam <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/24 18:16:46 by ynoam             #+#    #+#             */
+/*   Updated: 2019/10/24 20:03:34 by ynoam            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*returned;
+
+	i = 0;
+	while (s[i])
+		i++;
+	if (!(returned = (char *)malloc(i + 1)))
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		returned[i] = f(i, s[i]);
+		i++;
+	}
+	returned[i] = '\0';
+	return (returned);
+}
