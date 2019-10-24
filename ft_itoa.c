a0d8f2d84fabe9edc09d8cd1d6d09cb18397f058
Youssef Noam <ynoam@e1r1p7.1337.ma>
2019-10-24 18:06:51 +0200

    add ft_itoa.c

---
+++ ft_itoa.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ynoam <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/24 14:59:46 by ynoam             #+#    #+#             */
+/*   Updated: 2019/10/24 18:05:55 by ynoam            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	thefunlen(unsigned int copy)
+{
+	static int len;
+
+	while (copy > 9)
+	{
+		copy = copy / 10;
+		len++;
+	}
+	return (++len);
+}
+
+char		*ft_itoa(int n)
+{
+	char			*ptr;
+	unsigned int	copy;
+	int				i;
+	static int		sign;
+
+	if (n < 0)
+	{
+		copy = n * -1;
+		sign = 1;
+	}
+	else
+		copy = n;
+	i = thefunlen(copy);
+	if (!(ptr = malloc(i + sign + 1)))
+		return (NULL);
+	ptr[i + sign] = '\0';
+	while (copy > 9)
+	{
+		ptr[--i + sign] = (copy % 10) + 48;
+		copy /= 10;
+	}
+	ptr[--i + sign] = copy + 48;
+	return (ptr);
+}
