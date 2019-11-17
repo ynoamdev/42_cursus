01f1cf9c4eb68e6b0800125a9f26a0e3dea279da
Youssef Noam <ynoam@e1r4p12.1337.ma>
2019-11-17 18:57:49 +0100

    get_next_line

---
+++ get_next_line/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	char *line;
+
+	while(get_next_line(0, &line))
+		printf("%s", line);
+	return (0);
+}
+
+
+labhairi mouaad 
+
+rah tan3raf nktab bla manchouf\
+		yousef noan	
