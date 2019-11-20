b2572cbdd4bf7e4baef79b880befb1da485f5bb2
Youssef Noam <ynoam@e1r9p5.1337.ma>
2019-11-20 22:57:10 +0100

    1.06

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,22 +6,22 @@
 /*   By: ynoam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/16 15:18:39 by ynoam             #+#    #+#             */
-/*   Updated: 2019/11/19 23:12:15 by ynoam            ###   ########.fr       */
+/*   Updated: 2019/11/20 22:52:34 by ynoam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	ft_free(char *stk)
+int	ft_free(char *ptr)
 {
-	free(stk);
-	stk = NULL;
+	free(ptr);
+	ptr = NULL;
 	return (1);
 }
 
 int	get_next_line(int fd, char **line)
 {
-	ssize_t		r_read;
+	ssize_t		r;
 	char		*t;
 	static char	*stk;
 
@@ -29,23 +29,22 @@ int	get_next_line(int fd, char **line)
 		return (-1);
 	if (!(t = (char *)malloc(BUFFER_SIZE + 1)))
 		return (-1);
-	while (ft_search(stk) && (r_read = read(fd, t, BUFFER_SIZE)) != 0)
+	while ((r = 2) > 0 && ft_search(stk) && (r = read(fd, t, BUFFER_SIZE)) != 0)
 	{
-		if (r_read == -1 && ft_free(t))
+		if (r == -1 && ft_free(t) && ft_free(stk))
 			return (-1);
-		t[r_read] = '\0';
+		t[r] = '\0';
 		if (!(stk = ft_join(stk, t)) && ft_free(stk) && ft_free(t))
 			return (-1);
 	}
-	if (r_read != 0)
+	if (r != 0)
 		ft_free(t);
-	if (!(*line = ft_sub(stk)) && r_read != 0 && ft_free(stk))
+	if (!(*line = ft_sub(stk)) && ft_free(t) && ft_free(stk))
 		return (-1);
 	t = stk;
-	if (!(stk = ft_from_newline(stk)) && r_read != 0 && ft_free(stk))
+	if (!(stk = ft_from_newline(stk)) && ft_free(t) && ft_free(stk))
 		return (-1);
-	ft_free(t);
-	if (ft_free(stk))
+	if (r == 0 && ft_free(t) && ft_free(stk))
 		return (0);
 	return (1);
 }
