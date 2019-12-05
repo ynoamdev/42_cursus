198c5f98c517151e363498507b7f23d5b0a46d0a
Youssef Noam <ynoam@e1r9p5.1337.ma>
2019-12-05 19:06:05 +0100

    working

--- ft_printf/ft_all.c
+++ ft_printf/ft_all.c
@@ -6,7 +6,7 @@
 /*   By: ynoam <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/04 11:41:37 by ynoam             #+#    #+#             */
-/*   Updated: 2019/12/05 15:52:19 by ynoam            ###   ########.fr       */
+/*   Updated: 2019/12/05 19:04:40 by ynoam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,30 @@
 char	*ft_all(char **ptr, va_list ap)
 {
 	char	*s;
-	char	*p;
 	int		space;
 	int		zero;
 	int		dot;
+	int		menus;
 
 	s = ft_strdup("");
 	space = 0;
-	zero  = 0;
-	dot   = 0;
-	p = *ptr;
+	zero = 0;
+	dot = 0;
+	menus = 0;
 	if (ft_is_flage(**ptr))
 	{
 		s = ft_find_conv(*ptr, ap);
-		while(ft_is_flage(**ptr))
+		while (ft_is_flage(**ptr))
 		{
 			if (ft_isdigit(**ptr) && **ptr != '0')
-				space = space + (**ptr - 48);
-			else if(**ptr == '.' && *ptr++ && (dot = 1))
-				zero = zero + (**ptr - 48);
-			else if(ft_isdigit(**ptr) && **ptr == '0')
-				zero = zero + (**ptr - 48);
-			*ptr = *ptr + 1;
+				space +=  (**ptr - 48);
+			else if(**ptr == '*')
+				(**(ptr - 1) != '0') ? (space += va_arg(ap, int)) : (zero += va_arg(ap, int));
+			else if (**ptr == '.' || **ptr == '-')
+				(**ptr == '-') ? (menus = 1) : (dot = 1);
+			else if (ft_isdigit(**ptr) && **ptr == '0' && *ptr++ && (dot = 1))
+				zero += (**ptr - 48);
+			*ptr += 1;
 		}
 	}
 	else
