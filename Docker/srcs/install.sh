8c1e53141540d694c78c1a119ad0280b31e8ad91
Youssef Noam <ynoam@e1r3p3.1337.ma>
2020-02-01 21:14:14 +0100

    write a script bash to install all the package needed

--- Docker/srcs/install.sh
+++ Docker/srcs/install.sh
@@ -4,4 +4,8 @@ apt-get install -y man
 apt-get install -y curl
 apt-get install -y wget
 apt-get install -y nginx
-
+apt install -y php-json php-mbstring
+wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
+tar -zxvf phpMyAdmin-4.9.0.1-all-languages.tar.gz
+mv phpMyAdmin-4.9.0.1-all-languages/  /usr/share/phpMyAdmin
+rm -rf phpMyAdmin-4.9.0.1-all-languages.tar.gz
