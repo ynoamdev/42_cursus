66d8bcd418095ac9535a94094ba106aa8103f935
yssefnoam <2020noam@gmail.com>
2020-02-16 21:04:37 +0100

    step1 done with a litle bit of leak

--- minishell/shell/shelloop.c
+++ minishell/shell/shelloop.c
@@ -6,27 +6,33 @@
 /*   By: ynoam <ynoam@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/15 20:41:19 by ynoam             #+#    #+#             */
-/*   Updated: 2020/02/16 18:29:05 by root             ###   ########.fr       */
+/*   Updated: 2020/02/16 21:00:12 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libshell.h"
 
+/*
+** line 28 in for test.
+*/
+
 void	shelloop(void)
 {
-	char	**line;
+	char	**command;
+	char	*line;
 
-	ft_putstr_fd("welcome\n", 1);
 	while(1)
 	{
 		prompt();
-		line = ft_split(read_line(), ' ');
-		ft_putstr_fd(line[0], 1);
-		while(*line != NULL)
+		line = read_line();
+		command = ft_split(line, ' ');
+		ft_putstr_fd(command[0], 1);
+		while(*command != NULL)
 		{
-			free(*line);
-			line++;
+			free(*command);
+			command++;
 		}
+		free(*command);
 		free(line);
 	}
 }
