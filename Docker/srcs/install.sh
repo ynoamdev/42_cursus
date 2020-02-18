772202d692fc235d33f71d28ad8150b093850db5
Youssef Noam <ynoam@e1r3p3.1337.ma>
2020-02-18 22:46:11 +0100

    lkjlkjlkj

--- Docker/srcs/install.sh
+++ Docker/srcs/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-##  Installe vim man git curl wget gcc tmux ...
+##  Installe VIM MAN GIT CURL WGET GCC TMUX ...
 #
  apt-get install -y nginx
  apt-get install -y vim 
@@ -13,27 +13,38 @@
  apt-get install -y tmux
  apt-get install -y zip
  apt-get install -y ps
+ apt-get install -y expect
  apt install lsb-release
+ apt install -y lsb-release gnupg
+ wget http://repo.mysql.com/mysql-apt-config_0.8.13-1_all.deb
  mv .bashrc ..
  mv default.nginx /etc/nginx/sites-available/default
  
 #
-## Install php
+## Install PHP
 #
- apt install -y php7.3-fpm
- apt install -y php7.3-mysql
+ apt install -y php-fpm
+ apt install -y php-mysql
  apt update
 
 #
-## Installe Phpmyadmin
+## Installe PHPMYADMIN
 #
  bash phpmyadmin.sh
  apt update
 
 #
-## Installe wordpress
+## Installe WORDPRESS
 #
  bash wordpress.sh
  apt update
 
+#
+## Installe MySQL
+#
+ expect exp.exp
+ apt update
+
 rm -fr phpMyAdmin-4.9.4-all-languages.tar.gz
+rm -fr latest.tar.gz
+rm -fr mysql-apt-config_0.8.13-1_all.deb
