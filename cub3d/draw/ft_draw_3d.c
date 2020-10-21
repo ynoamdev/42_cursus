afeba106ca2e12254a230bb44b093e210ee66857
Youssef Noam <ynoam@e1r9p2.1337.ma>
2020-10-21 20:50:56 +0100

    tomoroww

--- cub3d/draw/ft_draw_3d.c
+++ cub3d/draw/ft_draw_3d.c
@@ -6,7 +6,7 @@
 /*   By: ynoam <ynoam@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/09 19:04:06 by ynoam             #+#    #+#             */
-/*   Updated: 2020/10/21 00:00:45 by ynoam            ###   ########.fr       */
+/*   Updated: 2020/10/21 20:50:22 by ynoam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_draw_3d(t_images *img, t_rays ray[])
 {
-	int		x;
+	int		i;
 	float	pro_wall_height;
 	float	per_distance;
 	float	proj_distance;
@@ -24,67 +24,47 @@ void	ft_draw_3d(t_images *img, t_rays ray[])
 	int		y;
 	int		color;
 
-	x = 0;
+	i = 0;
 
-	while (x < g_data.win_width)
+	while (i < g_data.win_width)
 	{
-		if ((per_distance = ray[x].distance * ft_cos(ray[x].ray_angle -
+		if ((per_distance = ray[i].distance * ft_cos(ray[i].ray_angle -
 						g_player.direction)) == 0.0)
 			per_distance = 1.0;
-
 		proj_distance = (g_data.win_width / 2) / ft_tan(FOV_ANGLE / 2);
-
 		pro_wall_height = (TILE_SIZE / per_distance) * proj_distance;
-
 		wall_strip_height = (int)pro_wall_height;
-
 		wall_top_pixel = (g_data.win_height / 2) - (wall_strip_height / 2);
 		wall_top_pixel = (wall_top_pixel < 0) ? 0 : wall_top_pixel;
-
 		wall_bottom_pixel = (g_data.win_height / 2) + (wall_strip_height / 2);
 		wall_bottom_pixel = (wall_bottom_pixel > g_data.win_height) ? g_data.win_height : wall_bottom_pixel;
-
 		y = 0;
 		while (y < wall_top_pixel && wall_top_pixel < g_data.win_width)
-			my_mlx_pixel_put(img, x, y++, g_data.ceill);
+			my_mlx_pixel_put(img, i, y++, g_data.ceill);
 
 		int texture_offset_x;
-		if (ray[x].was_hit_ver)
-			texture_offset_x = (int)ray[x].wall_hity % TILE_SIZE;
+		int texture_offset_y;
+		if (ray[i].was_hit_ver)
+			texture_offset_x = (int)ray[i].wall_hity % TILE_SIZE;
 		else
-			texture_offset_x = (int)ray[x].wall_hitx % TILE_SIZE;
-
+			texture_offset_x = (int)ray[i].wall_hitx % TILE_SIZE;
 		y = wall_top_pixel;
 		while (y < wall_bottom_pixel)
 		{
-			/*int distance_from_top = y + (wall_strip_height / 2) - ((g_data.win_height * TILE_SIZE)/ 2);*/
-//			int distance_from_top = y + (wall_strip_height / 2) * ((g_data.win_height)/ 2);
-
-			if (ray[x].was_hit_ver && ray[x].is_rayfacing_left)
-			{
-				int texture_offset_y = (y - wall_top_pixel) * (g_txtr_n.height / wall_strip_height);
-				color = g_txtr_n.addr[(g_txtr_n.width * texture_offset_y) + texture_offset_x];
-			}
-			else if (ray[x].was_hit_ver && !(ray[x].is_rayfacing_left))
-			{
-				int texture_offset_y = distance_from_top * (g_txtr_e.height / wall_strip_height);
-				color = g_txtr_e.addr[(g_txtr_e.width * texture_offset_y) + texture_offset_x];
-			}
-			else if (!ray[x].was_hit_ver && ray[x].is_rayfacing_up)
-			{
-				int texture_offset_y = distance_from_top * (g_txtr_s.height / wall_strip_height);
-				color = g_txtr_s.addr[(g_txtr_s.width * texture_offset_y) + texture_offset_x];
-			}
-			else
-			{
-				int texture_offset_y = distance_from_top * (g_txtr_w.height / wall_strip_height);
-				color = g_txtr_n.addr[(g_txtr_n.width * texture_offset_y) + texture_offset_x];
-			}
-			my_mlx_pixel_put(img, x, y++, color);
+			// north texture
+			texture_ my_mlx_pixel_get(i, y, 1);
+			// south texture
+			texture_ my_mlx_pixel_get(i, y, 2);
+			// west texture
+			texture_ my_mlx_pixel_get(i, y, 3);
+			// east texture
+			texture_ my_mlx_pixel_get(i, y, 4);
+			color = g_txtr_n.addr[(g_txtr_n.width * texture_offset_y) + texture_offset_x];
+			my_mlx_pixel_put(img, i, y++, color);
 		}
 		y = wall_bottom_pixel;
 		while (y < g_data.win_height)
-			my_mlx_pixel_put(img, x, y++, g_data.floor);
-		x++;
+			my_mlx_pixel_put(img, i, y++, g_data.floor);
+		i++;
 	}
 }
